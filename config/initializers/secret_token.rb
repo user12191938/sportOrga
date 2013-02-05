c9ae82ccbe11a6bcd80e6855581a27df90a13c26
Daniel Kraus <wichtig.danielkraus@hotmail.de>
2013-02-05 15:17:30 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SportOrga::Application.config.secret_token = '532f1d68fe964ffacc7e7abd7f6c04d28f092bdc74c6dc0259253a2f6261ccf5c9e2c0c523257e7b4f5a7e293d16710cff575cfc47de7d47070b8fb446d6fec7'
