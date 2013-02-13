75cebd7c963be7fd6bcd7f90e44857e561fd24ab
Daniel Kraus <wichtig.danielkraus@hotmail.de>
2013-02-13 16:18:07 +0100

    added secure password, implemented gravatar, sidebar

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130210183843) do
+ActiveRecord::Schema.define(:version => 20130213143755) do
 
   create_table "microposts", :force => true do |t|
     t.string   "content"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(:version => 20130210183843) do
     t.string   "email"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "salt"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
